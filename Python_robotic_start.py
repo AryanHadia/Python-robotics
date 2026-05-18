26d6f39e86f53366be45b1d3bead4f3c87c7df3a
AryanHadia <aryan.hd09@gmail.com>
2026-05-19 00:33:34 +0330

    Add files via upload

---
+++ Python_robotic_start.py
@@ -0,0 +1,6 @@
+"""
+My first step into Robotics with Python
+Goal: Learn how to control a robot with Python (later on Raspberry Pi)
+"""
+
+print("I am Aryan. I am starting my professional robotics journey with Python.")
