85f7185fa1afa7605520c4b56d004a6b53cc5700
AryanHadia <aryan.hd09@gmail.com>
2026-05-19 00:31:44 +0330

    Add files via upload

---
+++ Robot_moving/Moving.py
@@ -0,0 +1,20 @@
+# robot controll fuctions
+class R_move:
+    def Forward(self): # mopve forword
+        pass
+    
+    def backward(self): # move backward
+        pass
+
+    def right_turn(self): # move right
+        pass
+
+    def left_turn(self): # move left
+        pass
+
+    def U_turn(self): # turnin fully
+        pass
+
+"""
+moving robot by keyboard 
+"""
