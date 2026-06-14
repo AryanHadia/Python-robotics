28d44e68056c4f676c217d83ef9205a1a6049f23
AryanHadia <aryan.hd09@gmail.com>
2026-06-14 23:13:19 +0330

    Update robot.ino

--- Color_Follower/robot/robot.ino
+++ Color_Follower/robot/robot.ino
@@ -1,3 +1,7 @@
+
+int echo = 12;
+int trig = 11 ;
+
 int m1p1 = 3;
 int m1p2 = 5;
 int m2p1 = 6;
@@ -5,21 +9,48 @@ int m2p2 = 9;
 int CL = 4;
 int motor_speed;
 int scan_speed;
+int dist;
 
-void setup() {
-    motor_speed = 90;
-    scan_speed = 40;
+
+// geting distance
+int getDistance() {
+    digitalWrite(trig, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig, LOW);
+    
+    long duration = pulseIn(echo, HIGH);
+    int distance = duration * 0.034 / 2;
+    
+    if (distance == 0) distance = 100;
+    if (distance > 100) distance = 100;
+    
+    return distance;
+}
+
+
+void setup() { // adruino setup
+    motor_speed = 160;
+    scan_speed = 150;
     Serial.begin(9600);
+    // pinmodes
+    pinMode(echo, INPUT);
+    pinMode(trig, OUTPUT);
     pinMode(m1p1, OUTPUT);
     pinMode(m1p2, OUTPUT);
     pinMode(m2p1, OUTPUT);
     pinMode(m2p2, OUTPUT);
     pinMode(CL, OUTPUT);
-    stop();
+    stop(); // sttopign robot
+    delay(500);
     Serial.println("READY");   
 }
 
+
+
 void loop() {
+    dist = getDistance();
     if (Serial.available() > 0) {
         char cmd = Serial.read();
         while (Serial.available()) Serial.read();
@@ -39,10 +70,15 @@ void loop() {
 }
 
 void forward() {
-    analogWrite(m1p1, motor_speed);
-    analogWrite(m1p2, 0);
-    analogWrite(m2p1, motor_speed);
-    analogWrite(m2p2, 0);
+    if (dist < 20) { //stop
+        return;
+    }
+    else {
+      analogWrite(m1p1, motor_speed);
+      analogWrite(m1p2, 0);
+      analogWrite(m2p1, motor_speed);
+      analogWrite(m2p2, 0);
+    } 
 }
 
 void backward() {
